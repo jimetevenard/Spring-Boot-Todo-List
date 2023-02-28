226ade92cf08f72153307309fb5cd7794746379d
Jim ETEVENARD <jetevenard@oxiane.com>
2023-02-28 14:54:41 +0100

    Init - Spring Initializr

---
+++ src/main/java/com/example/demototo/DemoTotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demototo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTotoApplication.class, args);
+	}
+
+}
